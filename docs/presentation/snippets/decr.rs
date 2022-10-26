f1e2eeb27eea710506f06c1caf1f838df908c1d8
csicar <carsten.csiky@gmail.com>
2022-10-26 13:56:19 +0200

    wip

--- docs/presentation/snippets/decr.rs
+++ docs/presentation/snippets/decr.rs
@@ -1,7 +1,7 @@
-fn decr() -> ty!{ w : i32 | w >= 0 } {
+fn incr() -> ty!{ w : i32 | w > 0 } {
   // $\Gamma_1 = (\set{}, \text{true})$
-  let mut i = ... as ty!{ v: i32 | v > 0};
+  let mut i = ... as ty!{ v: i32 | v >= 0};
   // $\Gamma_2 = (\set{i \mapsto v}, v > 0)$
-  i = i - 1;
-  // $\Gamma_3 = (\set{i \mapsto {\red{v_2}}}, v > 0 \wedge {\red{v_2 \doteq v - 1}})$
+  i = i + 1;
+  // $\Gamma_3 = (\set{i \mapsto {\red{v_2}}}, v > 0 \wedge {\red{v_2 \doteq v + 1}})$
   i }
--- docs/presentation/snippets/strong-updates.rs
+++ docs/presentation/snippets/strong-updates.rs
@@ -1,9 +1,9 @@
 fn client() -> ty!{ v: i32 | v == 4 } {
-  let mut q = 3;
   let mut a = 2;       // a : $\{ v_1 : i32 \mid v_1 == 2 \}$
-  let mut b = &mut a;  // b : $\{ v_2 : &i32 \mid v_2 == &a \}$
+  let mut q = 3;       // q : $\{ v_2 : i32 \mid v_2 == 3 \}$
+  let mut b = &mut a;  // b : $\{ v_3 : &i32 \mid v_3 == &a \}$
   *b = 0;              // changes a's value and type
-  b = &mut q;          // b : $\{ v_2 : &i32 \mid v_2 == &q \}$
+  b = &mut q;          // b : $\{ v_4 : &i32 \mid v_4 == &q \}$
   *b = 4;              // changes q's value and type
   a
 }
